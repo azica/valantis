a3dc8e10d073c74da497e79914dfdac79d64b258
Самудинова <azicakcl@gmail.com>
2024-02-28 21:10:38 +0600

    changed the api url

--- src/store/services/constant.ts
+++ src/store/services/constant.ts
@@ -1,2 +1,2 @@
 export const BASE_API_URL = `${process.env.REACT_APP_API_URL}`;
-export const SHOP_API_URL = "http://api.valantis.store:40000/";
+export const SHOP_API_URL = "https://api.valantis.store:41000/";
